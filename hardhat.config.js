2a9cd1973f09be2cac2e6ec7d3a0833b44a56cd4
Issei Kumagai <48951453+IsseiBlockchain@users.noreply.github.com>
2022-10-17 17:11:47 +0800

    Configuration changes to various. Added upgrade deployment script.

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,10 +1,17 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("@nomiclabs/hardhat-ethers");
 require("@openzeppelin/hardhat-upgrades");
-//require("@nomiclabs/hardhat-etherscan");
 require("dotenv").config();
 module.exports = {
-  solidity: "0.8.4",
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
   defaultNetwork: "fxcore",
   networks: {
     fxcore: {
@@ -14,22 +21,3 @@ module.exports = {
     },
   },
 };
-
-// networks: {
-//   fxcore: {
-//     url: "https://testnet-fx-json-web3.functionx.io:8545",
-//     chainId: 530,
-//     accounts: [`0x${process.env.PRIVATE_KEY}`]
-//   }
-// }
-// module.exports = {
-//   solidity: "0.8.4",
-//   defaultNetwork: "fxcore",
-//   networks: {
-//     fxcore: {
-//       url: process.env.RPC_URL,
-//       chainId: process.env.CHAIN_ID,
-//       accounts: [process.env.PRIVATE_KEY],
-//     },
-//   },
-// };
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,9 +3,9 @@ const { ethers, upgrades } = require("hardhat");
 async function main() {
   const YieldFarmer = await ethers.getContractFactory("YieldFarmer");
   console.log("Deploying YieldFarmer contract...");
-  const rewardTokenAddress = "0x80b5a32e4f032b2a058b4f29ec95eefeeb87adcd";
-  const rewardPerBlock = 10000;
-  const startBlock = 5214238;
+  const rewardTokenAddress = "0x80b5a32e4f032b2a058b4f29ec95eefeeb87adcd"; // WFX token address
+  const rewardPerBlock = 270000000000000000; // in wei
+  const startBlock = 5214238; // block height
   const yieldFarmer = await upgrades.deployProxy(
     YieldFarmer,
     [rewardTokenAddress, rewardPerBlock, startBlock],
