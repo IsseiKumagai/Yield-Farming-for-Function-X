191acb7da1e54b52f9599c36798a1ce482e37bc1
Issei Kumagai <48951453+IsseiBlockchain@users.noreply.github.com>
2022-10-20 16:58:18 +0800

    updated deployment param

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,7 +4,7 @@ async function main() {
   const YieldFarmer = await ethers.getContractFactory("YieldFarmer");
   console.log("Deploying YieldFarmer contract...");
   const rewardTokenAddress = "0x80b5a32e4f032b2a058b4f29ec95eefeeb87adcd"; // WFX token address
-  const rewardPerBlock = 270000000000000000; // in wei
+  const rewardPerBlock = "270000000000000000"; // in wei
   const startBlock = 5214238; // block height
   const yieldFarmer = await upgrades.deployProxy(
     YieldFarmer,
